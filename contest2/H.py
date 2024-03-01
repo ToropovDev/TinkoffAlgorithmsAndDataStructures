f7ce9b2dfd7bf19b172602f33dc56c222f7fd444
ToropovDev <topopovdev@arseneyt.ru>
2024-03-01 23:35:16 +0500

    finished contest2

--- contest2/H.py
+++ contest2/H.py
@@ -1,20 +1,46 @@
 # H-Хорошие дни
 
-def nearest_smaller_left(n, arr):
-    res = [-1] * n
+def max_sum_min_subarray(n, arr):
+    prev_smaller = [-1] * n
     stack = []
-
     for i in range(n):
         while stack and arr[stack[-1]] >= arr[i]:
             stack.pop()
         if stack:
-            res[i] = stack[-1]
+            prev_smaller[i] = stack[-1]
         stack.append(i)
+
+    left = [-1] * n
+    stack = []
+    for i in range(n):
+        while stack and arr[stack[-1]] > arr[i]:
+            stack.pop()
+        if stack:
+            left[i] = stack[-1]
+        stack.append(i)
+
+    right = [-1] * n
+    stack = []
+    for i in range(n - 1, -1, -1):
+        while stack and arr[stack[-1]] >= arr[i]:
+            stack.pop()
+        if stack:
+            right[i] = stack[-1]
+        stack.append(i)
+
+    prefix_sums = [0] * (n+1)
+    for i in range(1, n+1):
+        prefix_sums[i] = prefix_sums[i-1] + arr[i-1]
+
+    res = 0
+    for i in range(n):
+        l = left[i] + 1
+        r = right[i] - 1 if right[i] != -1 else n-1
+        res = max(res, arr[i] * (prefix_sums[r+1] - prefix_sums[l] if l <= r else 0))
 
     return res
 
 
 n = int(input())
 arr = list(map(int, input().split()))
-result = nearest_smaller_left(n, arr)
-print(result)
+print(max_sum_min_subarray(n, arr))
