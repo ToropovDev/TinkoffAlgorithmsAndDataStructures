2aa7f5d6105d441d870380a76ed496245430c3d1
ToropovDev <topopovdev@arseneyt.ru>
2024-05-07 12:31:41 +0500

    Finished contest11

---
+++ contest11/E.py
@@ -0,0 +1,24 @@
+s = input().strip()
+n = len(s)
+packed = [[""] * n for _ in range(n)]
+
+for length in range(1, n + 1):
+    for left in range(n - length + 1):
+        right = left + length - 1
+        min_str = s[left:right + 1]
+        if length > 4:
+            for right1 in range(left, right):
+                left2 = right1 + 1
+                t = packed[left][right1] + packed[left2][right]
+                if len(t) < len(min_str):
+                    min_str = t
+            for p in range(1, length):
+                if length % p == 0:
+                    is_periodic = all(s[i] == s[i - p] for i in range(left + p, right + 1))
+                    if is_periodic:
+                        t = f"{length // p}({packed[left][left + p - 1]})"
+                        if len(t) < len(min_str):
+                            min_str = t
+        packed[left][right] = min_str
+
+print(packed[0][n - 1])
---
+++ contest11/F.py
@@ -0,0 +1,30 @@
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+s = sum(a) * 2
+res = []
+
+
+def f(pos=0, _sum=0, current=None):
+    if current is None:
+        current = []
+    global res
+    if pos == m:
+        if _sum == n:
+            if not res or len(current) < len(res):
+                res = current.copy()
+            return
+        return
+    f(pos + 1, _sum, current)
+    f(pos + 1, _sum + a[pos], current + [a[pos]])
+    f(pos + 1, _sum + a[pos] * 2, current + [a[pos], a[pos]])
+
+
+if s < n:
+    print(-1)
+else:
+    f()
+    if not res:
+        print(0)
+    else:
+        print(len(res))
+        print(*res)
